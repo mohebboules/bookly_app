3eb959aebe9847763414da57a34d6dfb02a7b7d1
Moheb Boules Guirges <mohebboules@gmail.com>
2025-01-26 23:52:19 +0200

    Create feautred books cubit

---
+++ lib/features/splash/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit(this.repo) : super(FeaturedBooksInitial());
+
+  final HomeRepo repo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await repo.fetchFeaturedBooks();
+    result.fold(
+      (failure) =>
+          emit(FeaturedBooksFailure(errorMessage: failure.errorMessage)),
+      (books) => emit(FeaturedBooksSuccess(books: books)),
+    );
+  }
+}
